f34fb5873b797295cab4913b962f60476637e827
forcespace <force.space@rambler.ru>
2022-04-10 21:25:59 +0300

    2.1

--- src/lab-2/2.1/index.ts
+++ src/lab-2/2.1/index.ts
@@ -7,94 +7,84 @@ type ProgramError = {
 }
 
 const ERRORS = {
-    OTHER_ERROR: {
-        code: 123,
-        message: "!!"
+    RANGE_ERROR: {
+        code: 1,
+        message: 'Value very big'
     }
-};
+}
 
 function printErrorsAndExit(error: ProgramError) {
-    console.log(error.message);
-    process.exit(error.code);
+    console.log(error.message)
+    process.exit(error.code)
 }
 
-function stringToArray(value: string, separator: string = " "): Array<string | never> {
-    let result: Array<string | never> = [];
-    if (value !== "") {
-        result = value.split(separator);
+function stringToArray(value: string, separator: string = ' '): Array<string | never> {
+    let result: Array<string | never> = []
+    if (value !== '') {
+        result = value.split(separator)
     }
-    return result;
+    return result
 }
 
-function isValidNumber(v: number): boolean {
-    return v < Number.MAX_SAFE_INTEGER && !isNaN(v);
+function isValidNumber(value: number): boolean {
+    return value < Number.MAX_SAFE_INTEGER && !isNaN(value)
 }
 
 function arrayOfStringToArrayOfNumber(values: Array<string>): Array<number> {
-    // return values.map((value: string) => parseFloat(value));
+    return values.map((value: string) => parseFloat(value))
 
-    const result: Array<number | never> = [];
-
-    values.forEach(value => {
-        const element = parseFloat(value);
-        if (isValidNumber(element)) {
-            result.push(element);
-        }
-        else {
-            printErrorsAndExit(ERRORS.OTHER_ERROR);
-        }
-    })
-
-    return result;
+    // const result: Array<number | never> = []
+    //
+    // values.forEach(value => {
+    //     const element = parseFloat(value)
+    //     if (isValidNumber(element)) {
+    //         result.push(element)
+    //     } else {
+    //         printErrorsAndExit(ERRORS.RANGE_ERROR)
+    //     }
+    // })
+    //
+    // return result
 }
 
 function getMinElementOfArray(array: Array<number>): number {
-    return Math.min(...array);
+    return Math.min(...array)
 }
 
 function multipleElementsOfArrayToMin(array: Array<number>): Array<number> {
-    const minElement = getMinElementOfArray(array);
+    const minElement = getMinElementOfArray(array)
 
-    // return array.map((value: number) => (value * minElement));
+    return array.map((value: number) => (value * minElement))
 
-    for (let i = 0; i < array.length; i++) {
-        const temp = array[i] * minElement;
-        if (isValidNumber(temp)) {
-            array[i] = temp;
-        }
-        else {
-            printErrorsAndExit(ERRORS.OTHER_ERROR);
-        }
-    }
-
-    return array;
+    // for (let i = 0; i < array.length; i++) {
+    //     const result = array[i] * minElement
+    //     if (isValidNumber(result)) {
+    //         array[i] = result
+    //     } else {
+    //         printErrorsAndExit(ERRORS.RANGE_ERROR)
+    //     }
+    // }
+    //
+    // return array
 }
 
-function sortArray(array: Array<number>, order: "ASC" | "DESC" = "ASC"): Array<number> {
-    return array.sort((a, b) => order === "ASC" ? a - b : b - a)
+function sortArray(array: Array<number>, order: 'ASC' | 'DESC' = 'ASC'): Array<number> {
+    return array.sort((a, b) => order === 'ASC' ? a - b : b - a)
 }
 
 function main(): void {
-    const separator = " ";
-    const typeOfSort = "ASC";
+    const separator = ' '
+    const typeOfSort = 'ASC'
 
     const readLineInterface: Interface = createInterface({input, output})
     readLineInterface.question('Hi: \n', (answer: string) => {
-        const arrayOfString = stringToArray(answer, separator);
-        const arrayOfNumber = arrayOfStringToArrayOfNumber(arrayOfString);
-
-        // const multipliedArray = multipleElementsOfArrayToMin(arrayOfNumber);
-        // console.log(multipliedArray);
-
-        multipleElementsOfArrayToMin(arrayOfNumber);
-        sortArray(arrayOfNumber, typeOfSort);
-
-        console.log(arrayOfNumber);
-        // readLineInterface.write(arrayOfNumber.join(" "));
-        readLineInterface.close();
-    });
-
-
+        const arrayOfString = stringToArray(answer, separator)
+        const arrayOfNumber = arrayOfStringToArrayOfNumber(arrayOfString)
+        const multipliedArray = multipleElementsOfArrayToMin(arrayOfNumber)
+        const sortedArray = sortArray(multipliedArray, typeOfSort)
+        console.log(sortedArray)
+        readLineInterface.close()
+    })
 }
 
-main();
+main()
