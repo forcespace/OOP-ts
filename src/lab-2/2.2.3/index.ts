741ca44b29018a1a3b5e47781fd8f64a0d6924ed
forcespace <force.space@rambler.ru>
2022-04-10 23:42:09 +0300

    2.2.3 - ?

---
+++ src/lab-2/2.2.3/index.ts
@@ -0,0 +1,58 @@
+import {createInterface, Interface} from 'node:readline'
+import {stdin as input, stdout as output} from 'node:process'
+
+const argv = process.argv
+
+type ProgramError = {
+    code: number;
+    message: string;
+}
+
+const ERRORS = {
+    EMPTY_INPUT: {
+        code: 1,
+        message: 'Error. Empty input.'
+    },
+    LESS_ARGUMENTS: {
+        code: 1,
+        message: 'Args so bad.'
+    }
+}
+
+function printErrorsAndExit(error: ProgramError) {
+    console.log(error.message)
+    process.exit(error.code)
+}
+
+function parseArgs(args: Array<string>): Array<string> {
+    if (args.length !== 4) {
+        printErrorsAndExit(ERRORS.LESS_ARGUMENTS)
+    }
+
+    return args
+}
+
+function getPositionInString(inputLine: string, searchLine: string): number {
+    return inputLine.search(searchLine)
+}
+
+function replaceInString(inputLine: string, searchValue: string, replaceValue: string): string {
+    return inputLine.replace(searchValue, replaceValue)
+}
+
+function main(arguments2: Array<string>): void {
+    // const readLineInterface: Interface = createInterface({input, output})
+    // readLineInterface.question('Enter string for find and replace: \n', (answer: string) => {
+        const args = parseArgs(arguments2)
+        // const readLineInterface: Interface = createInterface({input, output})
+        const findString = args[2]
+        const replaceString = args[3]
+        const text = 'any text'
+        const result = replaceInString(text, findString, replaceString)
+        console.log(result)
+
+        // readLineInterface.close()
+    // })
+}
+
+main(argv)
