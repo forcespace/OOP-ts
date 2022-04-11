c05acc6afb0e4d1748923460757e6f177590e298
forcespace <force.space@rambler.ru>
2022-04-11 18:52:55 +0300

    2.2.3

--- src/lab-2/2.2.3/index.ts
+++ src/lab-2/2.2.3/index.ts
@@ -16,6 +16,10 @@ const ERRORS = {
     LESS_ARGUMENTS: {
         code: 1,
         message: 'Args so bad.'
+    },
+    NO_MATCHES: {
+        code: 1,
+        message: 'No matches.'
     }
 }
 
@@ -32,8 +36,15 @@ function parseArgs(args: Array<string>): Array<string> {
     return args
 }
 
-function getPositionInString(inputLine: string, searchLine: string): number {
-    return inputLine.search(searchLine)
+// function getPositionInString(inputLine: string, searchLine: string): number {
+//     return inputLine.search(searchLine)
+// }
+
+function checkFullnessInputString(inputLine: string): string {
+    if (inputLine === '') {
+        printErrorsAndExit(ERRORS.EMPTY_INPUT)
+    }
+    return inputLine
 }
 
 function replaceInString(inputLine: string, searchValue: string, replaceValue: string): string {
@@ -41,18 +52,18 @@ function replaceInString(inputLine: string, searchValue: string, replaceValue: s
 }
 
 function main(arguments2: Array<string>): void {
-    // const readLineInterface: Interface = createInterface({input, output})
-    // readLineInterface.question('Enter string for find and replace: \n', (answer: string) => {
+    const readLineInterface: Interface = createInterface({input, output})
+    readLineInterface.question('Enter string for find and replace: \n', (answer: string) => {
         const args = parseArgs(arguments2)
-        // const readLineInterface: Interface = createInterface({input, output})
+        const readLineInterface: Interface = createInterface({input, output})
         const findString = args[2]
         const replaceString = args[3]
-        const text = 'any text'
-        const result = replaceInString(text, findString, replaceString)
+        const line = checkFullnessInputString(answer);
+        const result = replaceInString(line, findString, replaceString)
         console.log(result)
 
-        // readLineInterface.close()
-    // })
+        readLineInterface.close()
+    })
 }
 
 main(argv)
