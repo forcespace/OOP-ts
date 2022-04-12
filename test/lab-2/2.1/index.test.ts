9115251e9d9bd5ff50c0133c859d70e5854e58e2
forcespace <force.space@rambler.ru>
2022-04-12 20:43:58 +0300

    2.1

--- test/lab-2/2.1/index.test.ts
+++ test/lab-2/2.1/index.test.ts
@@ -7,15 +7,15 @@ describe('Each element of the array must be multiplied by a similar element of t
         expect(stringToArray('any text', ' ')).eql(['any', 'text'])
     })
     it('Converting a character to a number of float', () => {
-        expect(arrayOfStringToArrayOfNumber( ['1', '2', '1.2'])).eql([1, 2, 1.2])
+        expect(arrayOfStringToArrayOfNumber(['1', '2', '1.2'])).eql([1, 2, 1.2])
     })
-    it('Get the minimum element of an array', () => {
-        expect(getMinElementOfArray( [5, 2, 10.2])).eql(2)
+    it('Get the minimum element in an array', () => {
+        expect(getMinElementOfArray([5, 2, 10.2])).eql(2)
     })
     it('Multiply elements in an array by the minimum element of that array', () => {
-        expect(multipleElementsOfArrayToMin( [10, 20, 2, 30])).eql([20, 40, 4, 60])
+        expect(multipleElementsOfArrayToMin([10, 20, 2, 30])).eql([20, 40, 4, 60])
     })
     it('Sort array of ASC', () => {
-        expect(sortArray( [10, 20, 2, 30])).eql([2, 10, 20, 30])
+        expect(sortArray([10, 20, 2, 30])).eql([2, 10, 20, 30])
     })
 })
